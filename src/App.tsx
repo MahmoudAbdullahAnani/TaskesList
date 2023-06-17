9110015f572aa807a35ca1554da190946da358ca
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2023-06-17 18:31:36 +0300

    edit text area

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const time: string = currentDate.toLocaleTimeString('en-US', options);
           listTask.map(task => {
             return (
               <div key={task} className="perantTask" title={`${time}`} >
-                <textarea defaultValue={task} rows={6} cols={60}></textarea>
+                <textarea defaultValue={task} rows={6} cols={20}></textarea>
                 <div className="iconsInput">
                   <span onClick={() => {
                     const newList = listTask.filter(taskList => taskList !== task)
